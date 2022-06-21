a35a3c325f7c925f508b5710786dd92764f18643
piesieomane <piesieyeboah@gmail.com>
2022-06-21 18:10:02 +0300

    build the mobile menu

---
+++ menu.js
@@ -0,0 +1,16 @@
+const menuShow = document.querySelector("#menu");
+const xButton = document.querySelector(".xicon");
+
+menuShow.addEventListener("click", () => {
+  document.querySelector(".menu").classList.add("visible");
+});
+
+xButton.addEventListener("click", () => {
+  document.querySelector(".menu").classList.remove("visible");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) => {
+  n.addEventListener("click", () => {
+    document.querySelector(".menu").classList.remove("visible");
+  });
+});
